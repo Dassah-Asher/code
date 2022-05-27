15e59d9c5054ddd814576a74e5c4e2d11e2ea7cf
ashernuo <andassah@st.ug.edu.gh>
2022-05-27 12:24:44 +0000

    first commit

---
+++ src/com/example/nom/Nom.java
@@ -0,0 +1,13 @@
+package com.example.nom;
+import java.util.Scanner;
+
+
+public class Nom {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter your name");
+
+        String userName = myObj.nextLine(); 
+        System.out.println("Your name is " + userName);
+    }
+}
